d2e4ba76bbd6b272b39e7d5939e5e0994ebfd417
vxcj <msganosikelight@gmail.com>
2024-12-21 04:22:42 +0100

    loop through a list

---
+++ datascience.py
@@ -0,0 +1,7 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Loop through the list and print only even numbers
+for num in numbers:
+    if num % 2 == 0:  # Check if the number is even
+        print(num)
